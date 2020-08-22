85a6be76e4aa77e9662796c4365fd4ba9f82731d
Shiro Neko <tagiki5@gmail.com>
2020-08-22 14:30:28 -0300

    Web App completo

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface teacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<teacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://lh3.googleusercontent.com/ogw/ADGmqu85hoi7f7jFtyr1AWSMEQ-VH3je_F_CEO6KtCgK=s83-c-mo" alt="Taylor Hoffmann"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Taylor Hoffmann</strong>
-                            <span>Química</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
-                    <p>
-                        Entusiasta das melhores tecnologias de Química Avançada.
-                        <br/><br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma de minhas explosões.
-                    </p>
+                    <p>{teacher.bio}</p>
 
                     <footer>
                         <p>
                             Preço/Hora
-                            <strong>R$ 20,00</strong>
+                            <strong>R${teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a onClick={createNewConnection} href={`https://wa.me/55${teacher.whatsapp}`}>
                             <img src={whatsappIcon} alt="Whatsapp"/>
                             Entrar em Contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     );
