f35d77a6eba0998a33c30609c5c0143bbca8e734
Shiro Neko <tagiki5@gmail.com>
2020-08-22 22:07:55 -0300

    Território preparado para o desenvolvimento

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Olá Mundo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202024',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: '500',
+  }
 });
