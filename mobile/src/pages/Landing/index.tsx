ba3d9f65d20abf153d01707d071695f234724854
Shiro Neko <tagiki5@gmail.com>
2020-08-29 20:57:11 -0300

    Páginas de Proffys adicionada e navegação em abas

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -16,6 +16,10 @@ function Landing() {
         navigate('GiveClasses');
     }
 
+    function handleNavigateToStudyPages() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -27,7 +31,7 @@ function Landing() {
 
             <View style={styles.buttonsContainer}>
 
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
                 </RectButton>
