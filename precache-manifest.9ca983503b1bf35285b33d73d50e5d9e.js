685f1ca28f7d6465741830dea4758ac8d8f137f5
Shiro-Nek0 <tagiki5@gmail.com>
2020-09-08 13:59:19 -0300

    Updates

---
+++ precache-manifest.9ca983503b1bf35285b33d73d50e5d9e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a7353d81a4045a796f2032583e6fea",
+    "url": "/react-platform/index.html"
+  },
+  {
+    "revision": "88bc426ee0b8c9fa2f4c",
+    "url": "/react-platform/static/css/main.8528d453.chunk.css"
+  },
+  {
+    "revision": "21f5b369d830f59afb18",
+    "url": "/react-platform/static/js/2.e6e726c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-platform/static/js/2.e6e726c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bc426ee0b8c9fa2f4c",
+    "url": "/react-platform/static/js/main.38b5a9b9.chunk.js"
+  },
+  {
+    "revision": "2e6141c8593b4fde7829",
+    "url": "/react-platform/static/js/runtime-main.b32eef9d.js"
+  },
+  {
+    "revision": "1fbb1f8bd147351e311921cdf4e80dc3",
+    "url": "/react-platform/static/media/back.1fbb1f8b.svg"
+  },
+  {
+    "revision": "9ed76be57e0f41fb4092cbbcb15e0875",
+    "url": "/react-platform/static/media/give-classes.9ed76be5.svg"
+  },
+  {
+    "revision": "c86272eb642793deb29b7eec180c05a5",
+    "url": "/react-platform/static/media/landing.c86272eb.svg"
+  },
+  {
+    "revision": "1f9a96a14f86a1ccd682159fe038774d",
+    "url": "/react-platform/static/media/logo.1f9a96a1.svg"
+  },
+  {
+    "revision": "e042bb237d1a08daabd12bf0a4fb9780",
+    "url": "/react-platform/static/media/purple-heart.e042bb23.svg"
+  },
+  {
+    "revision": "3c22fb01ba7434dfa52b083a67158019",
+    "url": "/react-platform/static/media/study.3c22fb01.svg"
+  },
+  {
+    "revision": "744fb7ba0c3df74462352a3858badeb6",
+    "url": "/react-platform/static/media/warning.744fb7ba.svg"
+  },
+  {
+    "revision": "c2ffa8a6a2f6690a5eba826c625715ab",
+    "url": "/react-platform/static/media/whatsapp.c2ffa8a6.svg"
+  }
+]);
